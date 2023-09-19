a15eee0fd82215565d6329494509ac2398f8e20d
joaovitorpd <joaovitorpd@gmail.com>
2023-09-19 14:28:52 -0300

    Created and coded the medidor_circular widget dart file

---
+++ lib/widgets/medidor_circular.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class MedidorCircular extends StatefulWidget {
+  final double imc;
+
+  MedidorCircular({super.key, required this.imc});
+
+  @override
+  State<MedidorCircular> createState() => _MedidorCircularState();
+}
+
+class _MedidorCircularState extends State<MedidorCircular> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SfRadialGauge(
+        axes: <RadialAxis>[
+          RadialAxis(
+              minimum: 0.0,
+              maximum:
+                  50.0, // Defina o máximo de IMC de acordo com sua necessidade
+              ranges: <GaugeRange>[
+                GaugeRange(startValue: 0, endValue: 18.5, color: Colors.yellow),
+                GaugeRange(
+                    startValue: 18.5, endValue: 24.9, color: Colors.green),
+                GaugeRange(
+                    startValue: 25.0, endValue: 29.9, color: Colors.orange),
+                GaugeRange(startValue: 30.0, endValue: 60.0, color: Colors.red)
+              ],
+              pointers: <GaugePointer>[
+                NeedlePointer(
+                  value: widget.imc,
+                  needleColor: _getColorForIMC(widget.imc),
+                )
+              ],
+              annotations: <GaugeAnnotation>[
+                GaugeAnnotation(
+                    widget: Text(widget.imc.toString(),
+                        style: const TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold)),
+                    angle: 90,
+                    positionFactor: 0.5)
+              ]),
+        ],
+      ),
+    );
+  }
+
+  Color _getColorForIMC(double imc) {
+    if (imc < 18.5) {
+      return Colors.yellow;
+    } else if (imc <= 24.9) {
+      return Colors.green;
+    } else if (imc <= 29.9) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
+    }
+  }
+}
