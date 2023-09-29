2b4adaa18b1c1608db1d5fb2ba0bf7755d679c30
joaovitorpd <joaovitorpd@gmail.com>
2023-09-29 11:00:10 -0300

    Replaced unecessary Container and replaced for SizedBox in EntradaFormatada

--- lib/widgets/entrada_formatada.dart
+++ lib/widgets/entrada_formatada.dart
@@ -14,7 +14,7 @@ class EntradaFormatada extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width * 0.4,
       height: 50,
       child: TextFormField(
