e255f03c200f9e3148cc020a694728cc9176d612
joaovitorpd <joaovitorpd@gmail.com>
2023-10-05 10:32:09 -0300

    Card implemented in medidor_circular

--- lib/widgets/medidor_circular.dart
+++ lib/widgets/medidor_circular.dart
@@ -15,44 +15,48 @@ class MedidorCircular extends StatefulWidget {
 class _MedidorCircularState extends State<MedidorCircular> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(200, 255, 255, 255),
-        boxShadow: kElevationToShadow[4],
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: SfRadialGauge(
-          axes: <RadialAxis>[
-            RadialAxis(
-              minimum: 0.0,
-              maximum:
-                  50.0, // Defina o máximo de IMC de acordo com sua necessidade
-              ranges: <GaugeRange>[
-                GaugeRange(startValue: 0, endValue: 18.5, color: Colors.yellow),
-                GaugeRange(
-                    startValue: 18.5, endValue: 24.9, color: Colors.green),
-                GaugeRange(
-                    startValue: 25.0, endValue: 29.9, color: Colors.orange),
-                GaugeRange(startValue: 30.0, endValue: 60.0, color: Colors.red)
-              ],
-              pointers: <GaugePointer>[
-                NeedlePointer(
-                  value: widget.imc,
-                  needleColor: _getColorForIMC(widget.imc),
-                )
-              ],
-              /* annotations: <GaugeAnnotation>[
-                GaugeAnnotation(
-                    widget: Text(widget.imc.toStringAsFixed(2),
-                        style: const TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold)),
-                    angle: 90,
-                    positionFactor: 0.5),
-              ] */
-            ),
-          ],
+    return Card(
+      child: Container(
+        margin: const EdgeInsets.all(18.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(200, 255, 255, 255),
+          boxShadow: kElevationToShadow[4],
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+          child: SfRadialGauge(
+            axes: <RadialAxis>[
+              RadialAxis(
+                minimum: 0.0,
+                maximum:
+                    50.0, // Defina o máximo de IMC de acordo com sua necessidade
+                ranges: <GaugeRange>[
+                  GaugeRange(
+                      startValue: 0, endValue: 18.5, color: Colors.yellow),
+                  GaugeRange(
+                      startValue: 18.5, endValue: 24.9, color: Colors.green),
+                  GaugeRange(
+                      startValue: 25.0, endValue: 29.9, color: Colors.orange),
+                  GaugeRange(
+                      startValue: 30.0, endValue: 60.0, color: Colors.red)
+                ],
+                pointers: <GaugePointer>[
+                  NeedlePointer(
+                    value: widget.imc,
+                    needleColor: _getColorForIMC(widget.imc),
+                  )
+                ],
+                /* annotations: <GaugeAnnotation>[
+                  GaugeAnnotation(
+                      widget: Text(widget.imc.toStringAsFixed(2),
+                          style: const TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold)),
+                      angle: 90,
+                      positionFactor: 0.5),
+                ] */
+              ),
+            ],
+          ),
         ),
       ),
     );
