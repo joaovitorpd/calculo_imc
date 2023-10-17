86f3027936b568bea93e677cde2d8f57355f691b
joaovitorpd <joaovitorpd@gmail.com>
2023-10-17 11:37:59 -0300

    Implemented CartaoPadrao in EntradaCartao

--- lib/cards/entrada_cartao.dart
+++ lib/cards/entrada_cartao.dart
@@ -1,4 +1,5 @@
 import 'package:brasil_fields/brasil_fields.dart';
+import 'package:calculo_imc/cards/cartao_padrao.dart';
 import 'package:calculo_imc/widgets/botao_padrao_acao.dart';
 import 'package:calculo_imc/widgets/entrada_formatada.dart';
 import 'package:flutter/material.dart';
@@ -17,34 +18,30 @@ class EntradaCartao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.only(
-            top: 20.0, left: 5.0, right: 5.0, bottom: 20.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                EntradaFormatada(
-                  label: 'Peso (Kg)',
-                  controller: pesoController,
-                  formatter: PesoInputFormatter(),
-                ),
-                EntradaFormatada(
-                  label: 'Altura (metros)',
-                  controller: alturaController,
-                  formatter: AlturaInputFormatter(),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            BotaoPadraoAcao(
-                textoDoBotao: "Calcular IMC", funcaoDoBotao: calcularIMC),
-          ],
-        ),
+    return CartaoPadrao(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              EntradaFormatada(
+                label: 'Peso (Kg)',
+                controller: pesoController,
+                formatter: PesoInputFormatter(),
+              ),
+              EntradaFormatada(
+                label: 'Altura (metros)',
+                controller: alturaController,
+                formatter: AlturaInputFormatter(),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          BotaoPadraoAcao(
+              textoDoBotao: "Calcular IMC", funcaoDoBotao: calcularIMC),
+        ],
       ),
     );
   }
