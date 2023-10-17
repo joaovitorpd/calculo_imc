7341255f63260451dd8ee10bb3446e83311405bf
joaovitorpd <joaovitorpd@gmail.com>
2023-10-17 10:43:17 -0300

    Implemented Palette in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculo_imc/config/palette.dart';
 import 'package:calculo_imc/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,9 +17,10 @@ class AppCalculoIMC extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: const Locale('pt', 'BR'),
       theme: ThemeData(
-          primarySwatch: Colors.teal,
-          cardColor: const Color.fromRGBO(255, 255, 255, 1),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 193, 196, 202)),
+        primarySwatch: Palette.modoEscuro,
+        //cardColor: const Color.fromRGBO(255, 255, 255, 1),
+        //scaffoldBackgroundColor: const Color.fromARGB(255, 193, 196, 202)
+      ),
       home: const HomePage(title: 'Calcule seu IMC'),
     );
   }
