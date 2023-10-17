a102821f782bf0313002d3e7fa75fe6884f8b9ca
joaovitorpd <joaovitorpd@gmail.com>
2023-10-17 11:32:24 -0300

    Implemented CartaoPadrao.dart to fix the card's parameters

---
+++ lib/cards/cartao_padrao.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CartaoPadrao extends StatelessWidget {
+  final double elevation;
+  final Widget child;
+
+  const CartaoPadrao(
+      {super.key,
+      this.elevation = 5.0,
+      required this.child}); // Define um valor padrão de elevação
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation, // Usa o valor padrão ou o valor fornecido
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: child,
+      ),
+    );
+  }
+}
