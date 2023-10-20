f4f01291ba19cb963296ed33dd4edef0f1a130e4
joaovitorpd <joaovitorpd@gmail.com>
2023-10-20 10:51:35 -0300

    Updated layout to fix overflows and assure adaptability as Material's rules

--- lib/widgets/janela_de_ajuda.dart
+++ lib/widgets/janela_de_ajuda.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
 import 'package:flutter/material.dart';
 
 Future<void> janelaDeAjuda(BuildContext context) {
   return showDialog<void>(
     context: context,
     builder: (BuildContext context) {
+      String desenvolvedor = "João Vitor Pires Dias";
       return AlertDialog(
         title: Text(
           "Ajuda",
@@ -11,51 +14,56 @@ Future<void> janelaDeAjuda(BuildContext context) {
           style: Theme.of(context).textTheme.headlineSmall,
         ),
         content: SingleChildScrollView(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Expanded(
+          padding: const EdgeInsets.all(1.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              children: [
+                Wrap(
+                  children: [
+                    Text(
+                      """Um aplicativo para calcular o seu Índice de Massa Corpórea (IMC).
+Primeiro, insira o seu peso medido em Kilogramas (Kg), no formato ###,# (com uma casa decimal).
+Depois, insira sua altura, medida em metros (m), no formato #,## (com duas casas decimais).
+Por fim, clique no botão "Calcular", para obter seu IMC e classificação.""",
+                      textAlign: TextAlign.justify,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
                   child: Text(
-                    """Um aplicativo para calcular o seu Índice de Massa Corpórea (IMC).
-Primeiro, insira o seu peso medido em Kilogramas (Kg) no formato ###,# (com uma casa decimal).
-Depois, insira sua altura medida em metros (m), no formato #,## (com duas casas decimais).
-Por fim, clique no botão "Calcular" para obter seu IMC e classificação.""",
-                    textAlign: TextAlign.justify,
-                    style: Theme.of(context).textTheme.bodyMedium,
+                    "Desenvolvido por: ${desenvolvedor}",
+                    style: Theme.of(context).textTheme.labelMedium,
+                    textAlign: TextAlign.start,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Desenvolvido por: ",
-                    style: Theme.of(context).textTheme.labelMedium,
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Wrap(
+                    children: [
+                      Text(
+                        "Contato: ",
+                        style: Theme.of(context).textTheme.labelMedium,
+                        textAlign: TextAlign.start,
+                      ),
+                      Text(
+                        "joaovitorpd@gmail.com",
+                        style: Theme.of(context).textTheme.labelMedium,
+                        textAlign: TextAlign.end,
+                      ),
+                    ],
                   ),
-                  Text(
-                    "João Vitor",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Contato: ",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                  Text(
-                    "joaovitorpd@gmail.com",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
         actions: [
