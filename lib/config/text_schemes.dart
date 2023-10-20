4b4117fbe76ac2d297d3e20c11be50af57e73173
joaovitorpd <joaovitorpd@gmail.com>
2023-10-20 14:28:47 -0300

    Implemented TextTheme file

---
+++ lib/config/text_schemes.dart
@@ -0,0 +1,10 @@
+import 'package:calculo_imc/config/color_schemes.g.dart';
+import 'package:flutter/material.dart';
+
+TextTheme textSchemes = TextTheme(
+  labelMedium: TextStyle(
+    color: lightColorScheme.primary,
+  ),
+  headlineSmall: TextStyle(color: lightColorScheme.primary),
+  labelLarge: TextStyle(color: lightColorScheme.primary),
+);
