827d6af1e8f9d98a77867790fecf59394ade3a4e
joaovitorpd <joaovitorpd@gmail.com>
2023-10-20 11:53:57 -0300

    Fixed layout widget treee and constraints

--- lib/cards/medidor_circular.dart
+++ lib/cards/medidor_circular.dart
@@ -17,46 +17,51 @@ class _MedidorCircularState extends State<MedidorCircular> {
   @override
   Widget build(BuildContext context) {
     return CartaoPadrao(
-      child: Container(
-        margin: const EdgeInsets.all(18.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: kElevationToShadow[4],
-          shape: BoxShape.circle,
-        ),
-        child: Center(
-          child: SfRadialGauge(
-            axes: <RadialAxis>[
-              RadialAxis(
-                minimum: 0.0,
-                maximum:
-                    50.0, // Defina o máximo de IMC de acordo com sua necessidade
-                ranges: <GaugeRange>[
-                  GaugeRange(
-                      startValue: 0, endValue: 18.5, color: Colors.yellow),
-                  GaugeRange(
-                      startValue: 18.5, endValue: 24.9, color: Colors.green),
-                  GaugeRange(
-                      startValue: 25.0, endValue: 29.9, color: Colors.orange),
-                  GaugeRange(
-                      startValue: 30.0, endValue: 60.0, color: Colors.red)
-                ],
-                pointers: <GaugePointer>[
-                  NeedlePointer(
-                    value: widget.imc,
-                    needleColor: _getColorForIMC(widget.imc),
-                  )
-                ],
-                /* annotations: <GaugeAnnotation>[
-                  GaugeAnnotation(
-                      widget: Text(widget.imc.toStringAsFixed(2),
-                          style: const TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold)),
-                      angle: 90,
-                      positionFactor: 0.5),
-                ] */
-              ),
-            ],
+      child: SizedBox(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: Container(
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: kElevationToShadow[4],
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: SfRadialGauge(
+              axes: <RadialAxis>[
+                RadialAxis(
+                  minimum: 0.0,
+                  maximum: 60.0,
+                  showLastLabel:
+                      true, // Defina o máximo de IMC de acordo com sua necessidade
+                  ranges: <GaugeRange>[
+                    GaugeRange(
+                        startValue: 0, endValue: 18.5, color: Colors.yellow),
+                    GaugeRange(
+                        startValue: 18.5, endValue: 24.9, color: Colors.green),
+                    GaugeRange(
+                        startValue: 25.0, endValue: 29.9, color: Colors.orange),
+                    GaugeRange(
+                        startValue: 30.0, endValue: 60.0, color: Colors.red)
+                  ],
+                  pointers: <GaugePointer>[
+                    NeedlePointer(
+                      value: widget.imc,
+                      needleColor: _getColorForIMC(widget.imc),
+                    )
+                  ],
+                  /* annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        widget: Text(widget.imc.toStringAsFixed(2),
+                            style: const TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold)),
+                        angle: 90,
+                        positionFactor: 0.5),
+                  ] */
+                ),
+              ],
+            ),
           ),
         ),
       ),
